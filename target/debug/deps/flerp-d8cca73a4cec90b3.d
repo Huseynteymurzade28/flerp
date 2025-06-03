f2538b306d45c3d65285c1dd6be69c6fc7c9cc1d
flear <huseynteymurzade@marun.edu.tr>
2025-06-04 01:37:51 +0300

    let's start

---
+++ target/debug/deps/flerp-d8cca73a4cec90b3.d
@@ -0,0 +1,5 @@
+/home/flear/Desktop/MiniProjects/RustProject/flerp/target/debug/deps/flerp-d8cca73a4cec90b3.d: src/main.rs
+
+/home/flear/Desktop/MiniProjects/RustProject/flerp/target/debug/deps/libflerp-d8cca73a4cec90b3.rmeta: src/main.rs
+
+src/main.rs:
